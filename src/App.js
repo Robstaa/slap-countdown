c6a0c21f2f8a49fa13c14c4ad75308a7632c7024
Robin Zuschke <robin.l.zuschke@gmail.com>
2019-01-15 21:33:54 +0100

    Use timer component in App with hardcoded end-date

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Timer from './Timer/Timer';
+import moment from 'moment';
 
 const App = () => {
   return (
-    <div>Test</div>
+    <Timer timeDateGoal={moment("2019-01-18T22:06:07")} />
   );
 }
 
 export default App;
+
