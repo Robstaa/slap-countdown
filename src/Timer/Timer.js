86fac6b9f8bb50e316f6d9fa9bfb7192453044cc
Robin Zuschke <robin.l.zuschke@gmail.com>
2019-01-15 23:06:00 +0100

    Finalize Timer Component

--- src/Timer/Timer.js
+++ src/Timer/Timer.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import moment from 'moment';
+
+import './Timer.css';
 
 class Timer extends React.Component {
   state = {
@@ -13,41 +16,59 @@ class Timer extends React.Component {
     this.setStateOfCountdownItems();
     this.interval = setInterval(() => {
       this.setStateOfCountdownItems();
-    }, 1000)
+    }, 1000);
   }
 
-componentWillUnmount() {
-  clearInterval(this.interval);
-}
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
-setStateOfCountdownItems() {
-  const start = moment();
-  const end = this.props.timeDateGoal; 
-  const difference = moment.duration(end.diff(start));
-  const { days, hours, minutes, seconds } = difference._data;
-  this.setState({
-    days,
-    hours,
-    minutes,
-    seconds,
-  });
-}
+  setStateOfCountdownItems() {
+    const start = moment();
+    const end = moment(this.props.timeDateGoal);
+    const difference = moment.duration(end.diff(start));
+    const {
+      days, hours, minutes, seconds,
+    } = difference._data; // eslint-disable-line no-underscore-dangle
 
-render() {
-  const { days, hours, minutes, seconds } = this.state;
-  return(
-    <div className="timer-wrapper">
-    <div className="countdown-number">{days}</div>
-    <div className="countdown-text">Days</div>
-    <div className="countdown-number">{hours}</div>
-    <div className="countdown-text">Hours</div>
-    <div className="countdown-number">{minutes}</div>
-    <div className="countdown-text">Minutes</div>
-    <div className="countdown-number">{seconds}</div>
-    <div className="countdown-text">Seconds</div>
-    </div>
+    this.setState({
+      days,
+      hours,
+      minutes,
+      seconds,
+    });
+  }
+
+  render() {
+    const {
+      days, hours, minutes, seconds,
+    } = this.state;
+
+    return (
+      <div className='timer-wrapper'>
+        <div className="countdown-element-wrapper">
+          <div className='countdown-number'>{days}</div>
+          <div className='countdown-text'>Days</div>
+        </div>
+        <div className="countdown-element-wrapper">
+          <div className='countdown-number'>{hours}</div>
+          <div className='countdown-text'>Hours</div>
+        </div>
+        <div className="countdown-element-wrapper">
+          <div className='countdown-number'>{minutes}</div>
+          <div className='countdown-text'>Minutes</div>
+        </div>
+        <div className="countdown-element-wrapper">
+          <div className='countdown-number'>{seconds}</div>
+          <div className='countdown-text'>Seconds</div>
+        </div>
+      </div>
     );
+  }
 }
-}
+
+Timer.propTypes = {
+  timeDateGoal: PropTypes.string.isRequired,
+};
 
 export default Timer;
